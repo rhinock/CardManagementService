fdba17e7548f41071eea83b2b590327d1ff31673
rhinock <rhinock@yandex.ru>
2021-12-08 00:49:40 +0300

    Update ConfigController to use repository instead of static property

--- CMS/Controllers/ConfigController.cs
+++ CMS/Controllers/ConfigController.cs
@@ -1,13 +1,16 @@
 ﻿using CMS.Entities;
 using CMS.Extensions;
 using CMS.Models;
+using CMS.Repositories;
 using CMS.ResponseModels;
 using CMS.Types;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace CMS.Controllers
 {
@@ -17,11 +20,16 @@ namespace CMS.Controllers
     {
         private readonly IConfiguration _configuration;
         private readonly ILogger<ConfigController> _logger;
+        private readonly IRepository _repository;
 
-        public ConfigController(IConfiguration configuration, ILogger<ConfigController> logger)
+        public ConfigController(
+            IConfiguration configuration, 
+            ILogger<ConfigController> logger,
+            IRepository repository)
         {
             _logger = logger;
             _configuration = configuration;
+            _repository = repository;
         }
 
         /// <summary>
@@ -30,28 +38,34 @@ namespace CMS.Controllers
         /// <returns>Collection of cards or empty array if there are no cards</returns>
         [HttpGet]
         [ProducesResponseType(typeof(ResponseModel), 200)]
-        public IActionResult GetConfig()
+        public async Task<ActionResult> GetConfigAsync()
         {
             _logger.LogInformation("ConfigController.GetConfig");
 
-            if (CardCollection.Cards.Count == 0)
+            if (!_repository.Query<Card>().Any())
             {
-                CardCollection.Cards = _configuration.GetSection("Cards")
-                        .GetChildren()
-                        .Select(c => new Card()
-                        {
-                            Id = c.GetValue<Guid>("Id"),
-                            Cvc = c.GetValue<string>("Cvc"),
-                            Pan = c.GetValue<string>("Pan"),
-                            Expire = new Expire(c.GetValue<int>("Expire:Month"), c.GetValue<int>("Expire:Year")),
-                            Name = $"{_configuration["ASPNETCORE_ENVIRONMENT"]}_{c.GetValue<string>("Name")}",
-                            IsDefault = c.GetValue<bool>("IsDefault"),
-                            UserId = c.GetValue<Guid>("UserId")
-                        })
-                        .ToList<Card>();
+                var cardModels = _configuration.GetSection("Cards")?
+                    .GetChildren()?
+                    .Select(c => new CardModel()
+                    {
+                        Id = c.GetValue<Guid>("Id"),
+                        Cvc = c.GetValue<string>("Cvc"),
+                        Pan = c.GetValue<string>("Pan"),
+                        Expire = new Expire(c.GetValue<int>("Expire:Month"), c.GetValue<int>("Expire:Year")),
+                        Name = $"{_configuration["ASPNETCORE_ENVIRONMENT"]}_{c.GetValue<string>("Name")}",
+                        IsDefault = c.GetValue<bool>("IsDefault"),
+                        UserId = c.GetValue<Guid>("UserId")
+                    })?
+                    .ToList();
+
+                foreach (var cardModel in cardModels)
+                {
+                    Card card = cardModel.To<CardModel, Card>();
+                    await _repository.Create(card);
+                }
             }
 
-            return Info(CardCollection.Cards.Select(c => c.To<Card, CardModel>()));
+            return await InfoAsync(await _repository.Query<Card>().ToListAsync());
         }
     }
 }
