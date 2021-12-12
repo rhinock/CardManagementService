629770883c1fd5f339a2fed5a91c9f7a823d64a9
rhinock <rhinock@yandex.ru>
2021-12-12 03:14:01 +0300

    added WebTools extensions [2]

---
+++ CMSv2/CMS/WebTools/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Linq;
+
+namespace WebTools
+{
+    public static class Extensions
+    {
+        public static string GetErrorMessage(this ModelStateDictionary modelStateDictionary)
+        {
+            return modelStateDictionary
+                .Select(x => x.Value.Errors)
+                .Where(x => x.Count > 0)
+                .FirstOrDefault()?
+                .FirstOrDefault()?
+                .ErrorMessage;
+        }
+    }
+}
